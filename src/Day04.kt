b9c3636d8c555b62f3aa4acfa1d83251cd5ce9ab
Denys Yahofarov <denyago@gmail.com>
2021-12-04 21:31:58 +0100

    Day 4 improvements

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,6 +1,29 @@
 import Day04.asGame
 import Day04.play
 
+fun main() {
+  val noWinningBoardsYet: (game: Game) -> Boolean = { game -> game.winningBoards.isEmpty() }
+  val game1 = readInput("Day04_p1").asGame()
+  println(
+    "Part 1: " +
+            play(
+              game1,
+              shouldPlayAsLongAs = noWinningBoardsYet
+            ).winningBoard().score
+
+  ) // 4662
+
+  val notAllBoardsHaveWon: (game: Game) -> Boolean = { game -> game.boards.count(Board::isWinning) < game.boards.size }
+  val game2 = readInput("Day04_p1").asGame()
+  println(
+    "Part 2: " +
+            play(
+              game2,
+              shouldPlayAsLongAs = notAllBoardsHaveWon
+            ).winningBoard().score
+  ) // 12080
+}
+
 object Day04 {
   fun List<String>.asGame(): Game {
     val numberLine = this.first()
@@ -11,13 +34,9 @@ object Day04 {
     )
   }
 
-  fun play(game: Game, extraCondition: (game: Game) -> Boolean = { true }): Game =
-    if (game.shouldPlay()) {
-      if (extraCondition(game)) {
-        play(game.takeTurn(), extraCondition)
-      } else {
-        game
-      }
+  fun play(game: Game, shouldPlayAsLongAs: (game: Game) -> Boolean = { true }): Game =
+    if (game.canPlay() && shouldPlayAsLongAs(game)) {
+      play(game.takeTurn(), shouldPlayAsLongAs)
     } else {
       game
     }
@@ -32,73 +51,35 @@ data class Game(
     numbers.joinToString(", ") +
             "\n\n" +
             boards.mapIndexed { index, board ->
-              "Board $index (winning = ${board.isWinning}, score = ${board.score})\n${board.inspect()}"
+              board.inspect { "Board $index" }
             }.joinToString("\n\n")
 
   fun takeTurn() =
     if (numbers.isEmpty()) {
       this
     } else {
-      val newBoards = boards.map { it.maybeMark(numbers.first()) }
+      val theNumber = numbers.first()
+      val updatedBoards = boards.map { it.maybeMark(theNumber) }.groupBy { it.isWinning }
+      val restOfNumbers = numbers.drop(1)
       copy(
-        numbers = numbers.drop(1),
-        boards = newBoards.filterNot(Board::isWinning),
-        winningBoards = winningBoards + newBoards.filter(Board::isWinning)
+        numbers = restOfNumbers,
+        boards = updatedBoards.getOrDefault(false, emptyList()),
+        winningBoards = winningBoards + updatedBoards.getOrDefault(true, emptyList())
       )
     }
 
-  fun shouldPlay(): Boolean {
-    return numbers.isNotEmpty()
-  }
+  fun canPlay() = numbers.isNotEmpty()
 
   fun winningBoard(): Board =
     winningBoards.lastOrNull() ?: Board()
 }
 
-fun main() {
-  println(
-    "Part 1: " +
-            play(readInput("Day04_p1").asGame()) { game -> game.winningBoards.isEmpty() }.winningBoard().score
-
-  ) // 4662
-
-  println(
-    "Part 2: " +
-            play(readInput("Day04_p2").asGame()) { game -> game.boards.count(Board::isWinning) < game.boards.size }.winningBoard().score
-  ) // 12080
-}
 
 class Board {
-  var score: Int = 0
-  private val numbers = emptyMap<Key, BingoNumber>().toMutableMap()
-
-  data class Key(val columnIndex: Int, val rowIndex: Int)
-
+  var score = 0
   var isWinning = false
 
-  fun inspect() =
-    (0..4).joinToString("\n") { rowIndex ->
-      (0..4).joinToString("") { columnIndex ->
-        numbers.getOrDefault(key(columnIndex, rowIndex), BingoNumber(-1)).inspect()
-      }
-    }
-
-  companion object {
-    fun fromText(lines: List<String>) =
-      lines.foldIndexed(Board()) { rowIndex, board, line ->
-        line.trim().split("""\s+""".toRegex())
-          .map(BingoNumber::fromString)
-          .foldIndexed(board) { columnIndex: Int, _, bingoNumber: BingoNumber ->
-            board.updateAt(columnIndex, rowIndex, bingoNumber)
-          }
-      }
-  }
-
-  private fun updateAt(columnIndex: Int, rowIndex: Int, bingoNumber: BingoNumber) =
-    numbers.put(key(columnIndex, rowIndex), bingoNumber).let { this }
-
-  private fun key(columnIndex: Int, rowIndex: Int) =
-    Key(columnIndex, rowIndex)
+  private val numbers = emptyMap<Key, BingoNumber>().toMutableMap()
 
   fun maybeMark(number: Int): Board {
     numbers.forEach { (key, value) ->
@@ -113,11 +94,35 @@ class Board {
     return this
   }
 
+  fun inspect(name: (board: Board) -> String = { board -> "Board #${board.hashCode()}" }) =
+    name(this) + if (isWinning) " (had won with score $score)" else "" + "\n" +
+    (0..4).joinToString("\n") { rowIndex ->
+      (0..4).joinToString("") { columnIndex ->
+        numbers.getOrDefault(buildKey(columnIndex, rowIndex), BingoNumber(-1)).inspect()
+      }
+    }
+
+  companion object {
+    fun fromText(lines: List<String>) =
+      lines.foldIndexed(Board()) { rowIndex, board, line ->
+        line.trim().split("""\s+""".toRegex())
+          .map(BingoNumber::fromString)
+          .foldIndexed(board) { columnIndex: Int, _, bingoNumber: BingoNumber ->
+            board.numbers[buildKey(columnIndex, rowIndex)] = bingoNumber
+            board
+          }
+      }
+
+    private data class Key(val columnIndex: Int, val rowIndex: Int)
+    private fun buildKey(columnIndex: Int, rowIndex: Int) =
+      Key(columnIndex, rowIndex)
+  }
+
   private fun maybeWinning(key: Key): Boolean {
     val rawWinning =
-      (0..4).map { Key(it, key.rowIndex) }.count { numbers.getOrDefault(it, BingoNumber(-1)).isMarked } == 5
+      (0..4).map { buildKey(it, key.rowIndex) }.count { numbers.getOrDefault(it, BingoNumber(-1)).isMarked } == 5
     val columnWinning =
-      (0..4).map { Key(key.columnIndex, it) }.count { numbers.getOrDefault(it, BingoNumber(-1)).isMarked } == 5
+      (0..4).map { buildKey(key.columnIndex, it) }.count { numbers.getOrDefault(it, BingoNumber(-1)).isMarked } == 5
     return rawWinning || columnWinning
   }
 }
